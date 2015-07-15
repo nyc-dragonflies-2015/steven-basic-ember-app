ef95ab50b6a03534c222a7e599e7277c7fd06764
Steven G. Harms <steven@devbootcamp.com>
2015-07-15 03:39:38 -0400

    Return Items as a route

---
+++ app/controllers/items.js
@@ -0,0 +1,15 @@
+export default Ember.Controller.extend({
+  init: function() {
+    var c = this;
+    $.
+      getJSON('http://api.reddit.com')
+    .then(function(response) {
+      var elements =  response
+      .data
+      .children.map(function(f){
+        return f.data
+      });
+      c.set('model', elements);
+    });
+  }
+})
